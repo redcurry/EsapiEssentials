421acfdfa096dabf616e2cae012137823217e669
Carlos Anderson <carlosja@med.umich.edu>
2018-08-13 16:38:55 -0400

    Write a dummy dose metric calculation

--- src/EsapiPowerTools.DoseMetrics/DoseMetricCalculator.cs
+++ src/EsapiPowerTools.DoseMetrics/DoseMetricCalculator.cs
@@ -1,12 +1,38 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using EsapiPowerTools.Async;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
 
 namespace EsapiPowerTools.DoseMetrics
 {
     public class DoseMetricCalculator
     {
-        public Task<double> CalculateAsync(string planningItemId, string structureId, string metric)
+        private readonly Patient _patient;
+        private readonly EsapiAsyncRunner _asyncRunner;
+
+        public DoseMetricCalculator(Patient patient, EsapiAsyncRunner asyncRunner)
         {
-            return Task.FromResult(0.0);
+            _patient = patient;
+            _asyncRunner = asyncRunner;
+        }
+
+        public Task<double> CalculateAsync(string courseId, string planningItemId, string structureId, string metric)
+        {
+            // Return the mean dose for now; TODO: Check for nulls
+            return _asyncRunner.RunAsync(() =>
+            {
+                var course = _patient.Courses?.FirstOrDefault(x => x.Id == courseId);
+                var planningItem = course.PlanSetups?.FirstOrDefault(x => x.Id == planningItemId) as PlanningItem ??
+                                   course.PlanSums?.FirstOrDefault(x => x.Id == planningItemId);
+                Structure structure = null;
+                if (planningItem is PlanSetup plan)
+                    structure = plan.StructureSet.Structures.FirstOrDefault(x => x.Id == structureId);
+                else if (planningItem is PlanSum planSum)
+                    structure = planSum.StructureSet.Structures.FirstOrDefault(x => x.Id == structureId);
+                var dvhResult = planningItem.GetDVHCumulativeData(structure, DoseValuePresentation.Absolute, VolumePresentation.AbsoluteCm3, 0.001);
+                return dvhResult.MeanDose.Dose;
+            });
         }
     }
 }
