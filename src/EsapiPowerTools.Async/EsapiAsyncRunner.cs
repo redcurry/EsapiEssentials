1bf2586ef5399edaa57cfd35445deb521fb7f4ac
Carlos Anderson <carlosja@med.umich.edu>
2018-08-13 16:16:14 -0400

    Implement EsapiAsyncRunner using StaTaskScheduler

---
+++ src/EsapiPowerTools.Async/EsapiAsyncRunner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Schedulers;
+
+namespace EsapiPowerTools.Async
+{
+    public class EsapiAsyncRunner : IDisposable
+    {
+        private readonly StaTaskScheduler _taskScheduler;
+
+        public EsapiAsyncRunner()
+        {
+            _taskScheduler = new StaTaskScheduler(1);
+        }
+
+        public Task RunAsync(Action a) =>
+            Task.Factory.StartNew(a, CancellationToken.None, TaskCreationOptions.None, _taskScheduler);
+
+        public Task<TResult> RunAsync<TResult>(Func<TResult> f) =>
+            Task.Factory.StartNew(f, CancellationToken.None, TaskCreationOptions.None, _taskScheduler);
+
+        public void Dispose()
+        {
+            _taskScheduler?.Dispose();
+        }
+    }
+}
