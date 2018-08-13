b739a12d0f720019a0e0689a38c5361e8ed1b30b
Carlos Anderson <carlosja@med.umich.edu>
2018-08-13 16:00:24 -0400

    Add DoseMetrics project and initial class

---
+++ src/EsapiPowerTools.DoseMetrics/DoseMetricCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace EsapiPowerTools.DoseMetrics
+{
+    public class DoseMetricCalculator
+    {
+        public Task<double> CalculateAsync(string planningItemId, string structureId, string metric)
+        {
+            return Task.FromResult(0.0);
+        }
+    }
+}
---
+++ src/EsapiPowerTools.DoseMetrics/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EsapiPowerTools.DoseMetrics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michigan Medicine Radiation Oncology")]
+[assembly: AssemblyProduct("EsapiPowerTools.DoseMetrics")]
+[assembly: AssemblyCopyright("Copyright © Michigan Medicine Radiation Oncology 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("770f685b-807d-403f-ae48-6dd697c25510")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
