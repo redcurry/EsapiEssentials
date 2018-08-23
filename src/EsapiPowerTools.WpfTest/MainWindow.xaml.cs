cbf2edcc436cf70f9a145108fc747d8c82686986
Carlos Anderson <carlosja@med.umich.edu>
2018-08-23 12:54:56 -0400

    Add status text and progress bar

--- src/EsapiPowerTools.WpfTest/MainWindow.xaml.cs
+++ src/EsapiPowerTools.WpfTest/MainWindow.xaml.cs
@@ -6,22 +6,37 @@ namespace EsapiPowerTools.WpfTest
 {
     public partial class MainWindow : Window
     {
-        private EsapiService _esapiService;
+        private readonly EsapiService _esapiService;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            _esapiService = new EsapiService();
         }
 
         private async void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            _esapiService = new EsapiService();
+            ProgressBar.IsIndeterminate = true;
+
+            StatusTextBlock.Text = "Initializing ESAPI...";
             await _esapiService.LogInAsync("SysAdmin", "SysAdmin");
+
+            StatusTextBlock.Text = "Opening patient...";
             await _esapiService.OpenPatientAsync("$DVHAnalysisQA");
+
+            StatusTextBlock.Text = "Fetching courses...";
             var courseIds = await _esapiService.GetCourseIdsAsync();
+
             ListBox.ItemsSource = courseIds;
+
+            StatusTextBlock.Text = "Calculating metric...";
             var metric = await _esapiService.CalculateMetricAsync(null, "HEADNECK", "HN IMRT ECL", "CORD");
+
             TextBlock.Text = metric.ToString("F2");
+
+            StatusTextBlock.Text = "Done.";
+            ProgressBar.IsIndeterminate = false;
         }
 
         private void MainWindow_OnClosed(object sender, EventArgs e)
