21bfea4438f7ca9c2ff2aa90feacc61aa335be94
Carlos Anderson <carlosja@med.umich.edu>
2018-08-26 17:33:36 -0400

    Add Extensions project and PlanningItem extensions

---
+++ src/EsapiEssentials.Extensions/PlanningItemExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using VMS.TPS.Common.Model.API;
+
+namespace EsapiEssentials.Extensions
+{
+    public static class PlanningItemExtensions
+    {
+        public static Course GetCourse(this PlanningItem planningItem)
+        {
+            if (planningItem is PlanSetup plan)
+                return plan.Course;
+
+            if (planningItem is PlanSum planSum)
+                return planSum.Course;
+
+            throw new ArgumentException(GetUnknownSubclassMessage(planningItem));
+        }
+
+        public static StructureSet GetStructureSet(this PlanningItem planningItem)
+        {
+            if (planningItem is PlanSetup plan)
+                return plan.StructureSet;
+
+            if (planningItem is PlanSum planSum)
+                return planSum.StructureSet;
+
+            throw new ArgumentException(GetUnknownSubclassMessage(planningItem));
+        }
+
+        private static string GetUnknownSubclassMessage(PlanningItem planningItem) =>
+            $"{planningItem.GetType()} is not a known subclass of {nameof(PlanningItem)}.";
+    }
+}
