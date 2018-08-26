72f1df147693624904698a1f47d1df5358f84467
Carlos Anderson <carlosja@med.umich.edu>
2018-08-26 18:09:04 -0400

    Add documentation comments to extensions

--- src/EsapiEssentials.Extensions/PlanningItemExtensions.cs
+++ src/EsapiEssentials.Extensions/PlanningItemExtensions.cs
@@ -3,8 +3,16 @@ using VMS.TPS.Common.Model.API;
 
 namespace EsapiEssentials.Extensions
 {
+    /// <summary>
+    /// Extension methods for the PlanningItem class.
+    /// </summary>
     public static class PlanningItemExtensions
     {
+        /// <summary>
+        /// Gets the parent Course of the given PlanningItem.
+        /// </summary>
+        /// <param name="planningItem">The PlanningItem whose Course will be obtained.</param>
+        /// <returns>The parent Course of the given PlanningItem.</returns>
         public static Course GetCourse(this PlanningItem planningItem)
         {
             if (planningItem is PlanSetup plan)
@@ -16,6 +24,11 @@ namespace EsapiEssentials.Extensions
             throw new ArgumentException(GetUnknownSubclassMessage(planningItem));
         }
 
+        /// <summary>
+        /// Gets the StructureSet of the given PlanningItem.
+        /// </summary>
+        /// <param name="planningItem">The PlanningItem whose StructureSet will be obtained.</param>
+        /// <returns>The StructureSet of the given PlanningItem.</returns>
         public static StructureSet GetStructureSet(this PlanningItem planningItem)
         {
             if (planningItem is PlanSetup plan)
