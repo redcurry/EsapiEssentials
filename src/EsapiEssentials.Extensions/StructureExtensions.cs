4f6fabc2a00af6ca8e61c758f88f3c3eee86ea2a
Carlos Anderson <carlosja@med.umich.edu>
2018-08-26 17:57:23 -0400

    Add Structure extensions

---
+++ src/EsapiEssentials.Extensions/StructureExtensions.cs
@@ -0,0 +1,13 @@
+﻿using VMS.TPS.Common.Model.API;
+
+namespace EsapiEssentials.Extensions
+{
+    public static class StructureExtensions
+    {
+        public static bool IsTarget(this Structure structure) =>
+            structure.DicomType == "GTV" || structure.DicomType == "CTV" || structure.DicomType == "PTV";
+
+        public static bool IsOrgan(this Structure structure) =>
+            structure.DicomType == "ORGAN";
+    }
+}
