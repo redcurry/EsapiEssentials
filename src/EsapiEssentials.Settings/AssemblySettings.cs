639baf9d203bd45e2c274ac625f07758c9939211
Carlos Anderson <carlosja@med.umich.edu>
2018-08-27 18:58:18 -0400

    Add Settings project with AssemblySettings class

---
+++ src/EsapiEssentials.Settings/AssemblySettings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+
+namespace EsapiEssentials.Settings
+{
+    /// <summary>
+    /// Provides read-only access to an assembly's settings.
+    /// The settings must be in the assembly's App.config file,
+    /// in a section called appSettings (see the Microsoft documentation).
+    /// </summary>
+    internal class AssemblySettings
+    {
+        private readonly Assembly _assembly;
+
+        /// <summary>
+        /// Instantiates a new instance of the AssemblySettings class.
+        /// </summary>
+        /// <param name="assembly">The assembly that has the settings.</param>
+        public AssemblySettings(Assembly assembly)
+        {
+            _assembly = assembly;
+        }
+
+        /// <summary>
+        /// Gets the value of the setting with the given key (i.e., name).
+        /// </summary>
+        /// <param name="key">The name of the setting.</param>
+        /// <returns>The value of the setting.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// An exception occured while trying to obtain the setting.
+        /// </exception>
+        public string GetSetting(string key)
+        {
+            try
+            {
+                var asmPath = _assembly.Location;
+                var config = ConfigurationManager.OpenExeConfiguration(asmPath);
+                var setting = config.AppSettings.Settings[key];
+                return setting.Value;
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException("Unable to read the configuration setting.", e);
+            }
+        }
+    }
+}
---
+++ src/EsapiEssentials.Settings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EsapiEssentials.Settings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michigan Medicine Radiation Oncology")]
+[assembly: AssemblyProduct("EsapiEssentials.Settings")]
+[assembly: AssemblyCopyright("Copyright © Michigan Medicine Radiation Oncology 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c67073f-74eb-4473-b046-9a0532ce5451")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
