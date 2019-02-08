25e7adbb400c504e8672a9316a0ba40f60472b1b
Carlos Anderson <carlosja@med.umich.edu>
2019-02-08 13:56:49 -0500

    Remove old projects that were moved

--- src/EsapiEssentials.Search/Internal/SearchPatient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EsapiEssentials.Search.Internal
-{
-    internal class SearchPatient
-    {
-        public string Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
