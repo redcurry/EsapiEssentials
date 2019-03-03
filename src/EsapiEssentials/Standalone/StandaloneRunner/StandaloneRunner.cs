44a2e275a5197897d5fe65c769e456775d3ddfdf
Carlos Anderson <carlosja@med.umich.edu>
2019-03-03 17:53:39 -0500

    Write doc comments for the StandaloneRunner

--- src/EsapiEssentials/Standalone/StandaloneRunner/StandaloneRunner.cs
+++ src/EsapiEssentials/Standalone/StandaloneRunner/StandaloneRunner.cs
@@ -7,8 +7,24 @@ using VMS.TPS.Common.Model.API;
 
 namespace EsapiEssentials.Standalone
 {
+    /// <summary>
+    /// Allows a binary plugin script to run a standalone application,
+    /// while preserving the script context (sent via command-line arguments).
+    /// The plugin script assembly must be in the same directory as the
+    /// standalone application. One way to do that is to reference
+    /// the script project from the standalone application project.
+    /// </summary>
     public class StandaloneRunner
     {
+        /// <summary>
+        /// Run the standalone application with the given script context.
+        /// </summary>
+        /// <param name="context">The script context from the plugin script.</param>
+        /// <exception cref="StandaloneRunnerException">
+        /// An exception occurred while trying to run the standalone application.
+        /// Perhaps the application was not found or an error occurred while
+        /// building the command-line arguments from the script context.
+        /// </exception>
         public static void RunWith(ScriptContext context)
         {
             try
