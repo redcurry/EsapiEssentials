55fef581398b04a38f21cac5c3fa329d413b6931
Carlos Anderson <carlosja@med.umich.edu>
2019-03-05 17:17:35 -0500

    Add sample script to show usage of ScriptBase

---
+++ samples/EsapiEssentials.Samples.PluginScript.Runner/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using EsapiEssentials.PluginRunner;
+using VMS.TPS;
+
+namespace EsapiEssentials.Samples.PluginScript.Runner
+{
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            // Note: EsapiEssentials and EsapiEssentials.PluginRunner must be referenced,
+            // as well as the project that contains the Script class
+            ScriptRunner.Run(new Script());
+        }
+    }
+}
---
+++ samples/EsapiEssentials.Samples.PluginScript/Script.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using EsapiEssentials.Plugin;
+using VMS.TPS.Common.Model.Types;
+
+namespace VMS.TPS
+{
+    public class Script : ScriptBase
+    {
+        public override void Execute(PluginScriptContext context)
+        {
+            // Define the desired structure ID
+            var structureId = "Bladder";
+
+            // Find the structure in the current structure set
+            var structure = context?.StructureSet?.Structures?.FirstOrDefault(x => x.Id == structureId);
+
+            if (structure == null)
+                throw new InvalidOperationException("Unable to find the structure.");
+
+            // Calculate D2cc[Gy]
+            var d2cc = context?.PlanSetup?.GetDoseAtVolume(structure, 2, VolumePresentation.AbsoluteCm3, DoseValuePresentation.Absolute);
+
+            if (d2cc == null)
+                throw new InvalidOperationException("Unable to calculate the dose metric.");
+
+            // Show the result
+            MessageBox.Show($"{structureId} D2cc[Gy] = {d2cc}");
+        }
+    }
+}
