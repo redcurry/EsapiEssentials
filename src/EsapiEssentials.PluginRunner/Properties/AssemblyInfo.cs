10d11fdc5e9a29a49c7cb44f88b373ceb3a5f708
Carlos Anderson <carlosja@med.umich.edu>
2019-03-31 15:10:43 -0400

    Add copyright info to PluginRunner

--- src/EsapiEssentials.PluginRunner/Properties/AssemblyInfo.cs
+++ src/EsapiEssentials.PluginRunner/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EsapiEssentials.PluginRunner")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Plugin runner for EsapiEssentials")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Carlos Anderson")]
 [assembly: AssemblyProduct("EsapiEssentials.PluginRunner")]
 [assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
