4ed0f288ca42d65c2f30330c87847b27d2ba0b4b
Carlos Anderson <carlosja@med.umich.edu>
2019-07-01 17:13:56 -0400

    Bump version to 2.0.0.0 and update .nuspec

--- src/EsapiEssentials/Properties/AssemblyInfo.cs
+++ src/EsapiEssentials/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EsapiEssentials")]
-[assembly: AssemblyDescription("Essential tools for working with ESAPI")]
+[assembly: AssemblyDescription("Essential tools for working with ESAPI 15.6")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Carlos Anderson")]
 [assembly: AssemblyProduct("EsapiEssentials")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: InternalsVisibleTo("EsapiEssentials.UnitTests")]
